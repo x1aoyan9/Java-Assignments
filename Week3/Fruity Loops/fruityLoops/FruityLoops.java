6d799d3e2029450f43d2eec73cfeb3715b803abb
Sarah Lynn <98367848+x1aoyan9@users.noreply.github.com>
2022-07-24 23:49:06 -0500

    Add files via upload

---
+++ Week3/Fruity Loops/fruityLoops/FruityLoops.java
@@ -0,0 +1,11 @@
+package com.java.fruityLoops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruityLoops {
+    public static void main(String[] args) {
+        SpringApplication.run(FruityLoops.class, args);
+    }
+}
