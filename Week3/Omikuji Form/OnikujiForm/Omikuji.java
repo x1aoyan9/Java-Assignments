873fe93beeed2fd0bdf359506147014812e00b2d
Sarah Lynn <98367848+x1aoyan9@users.noreply.github.com>
2022-07-25 00:05:24 -0500

    Add files via upload

---
+++ Week3/Omikuji Form/OnikujiForm/Omikuji.java
@@ -0,0 +1,11 @@
+package com.java.omikuji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Omikuji {
+    public static void main(String[] args) {
+        SpringApplication.run(Omikuji.class, args);
+    }
+}
---
+++ Week3/Omikuji Form/OnikujiForm/test/OmikujiTest.java
@@ -0,0 +1,11 @@
+package com.java.omikuji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmikujiTest {
+    @Test
+    void contextLoads() {
+    }
+}
